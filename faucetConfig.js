76a4df505b93aa4975208e85db5795f30fa777ff
BurstNeon | T.W <billypwnz@gmail.com>
2018-07-26 13:18:01 +0200

    Create faucetConfig.js

---
+++ faucetConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+//Google Recaptcha: Public
+googlePublicKey : "",
+//Google Recaptcha: Private
+googlePrivateKey : "",
+//Claim Reward
+claimRewardNQT: 0.1,
+//Claim Fee
+claimFeeNQT: 0.1,
+//Local Wallet xxx.xxx.xxx:XXXX
+faucetWallet : "",
+//Passphrase for Faucet Account
+passphrase: "",
+//Numeric or RS-Address
+faucetAccount: "",
+}
